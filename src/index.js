9a443d58929b827d4328733a027afa5528106da0
leewellmacharia10@gmail.com <leewellmacharia10@gmail.com>
2022-09-14 17:40:56 +0300

    styling our jokes form

--- src/index.js
+++ src/index.js
@@ -7,3 +7,20 @@ function updateLikeColor() {
 document.getElementById('submit').addEventListener('click', () => alert('Successfully Submitted'))
 
 document.querySelector(".btn").addEventListener('click', () =>alert('Someone clicked me'))
+
+
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+//         let getJoke = document.querySelector(".btn")
+//         getJoke.addEventListener('click', fetch('https://dad-jokes.p.rapidapi.com/random/joke', options)
+//         .then(response => response.json())
+//         .then(response => console.log(response))
+//         .catch(err => console.error(err)))
+//         console.log(response)
+// 		'X-RapidAPI-Key': 'bd19915a35mshead8ae7b5e30360p1e137bjsned637b7bd883',
+// 		'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
+// 	}
+// };
+const init = () 
