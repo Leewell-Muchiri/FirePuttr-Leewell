4eea3f8f6a76fffac91382f3a7dd93b4b63752f1
leewellmacharia10@gmail.com <leewellmacharia10@gmail.com>
2022-09-14 17:02:41 +0300

    like button turns red on click

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 document.querySelector("#like").addEventListener('click',  () => alert('I made you laugh!'))
+
+document.getElementById('like').addEventListener('click', updateLikeColor)
+function updateLikeColor() {
+    document.getElementById('like').style.backgroundColor = "red";
+}
