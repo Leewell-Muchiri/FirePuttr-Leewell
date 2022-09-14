9f6c41a3b4bdb2dc22469908c47fa79deb3f0e9d
leewellmacharia10@gmail.com <leewellmacharia10@gmail.com>
2022-09-14 18:10:55 +0300

    changes to joke form

--- src/index.js
+++ src/index.js
@@ -4,23 +4,35 @@ document.getElementById('like').addEventListener('click', updateLikeColor)
 function updateLikeColor() {
     document.getElementById('like').style.backgroundColor = "red";
 }
+
 document.getElementById('submit').addEventListener('click', () => alert('Successfully Submitted'))
 
 document.querySelector(".btn").addEventListener('click', () =>alert('Someone clicked me'))
 
+// const getJoke = document.querySelector(".btn").addEventListener('click', e => {
+//     e.preventDefault();
+//     const input = document.querySelector('input#joke');
+//     console.log(input.value);
+//     fetch('https://dad-jokes.p.rapidapi.com/random/joke', options)
+//     .then(response => response.json())
+//      .then(response => console.log(response))
+// .catch(err => console.error(err)))
 
 
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-//         let getJoke = document.querySelector(".btn")
-//         getJoke.addEventListener('click', fetch('https://dad-jokes.p.rapidapi.com/random/joke', options)
-//         .then(response => response.json())
-//         .then(response => console.log(response))
-//         .catch(err => console.error(err)))
-//         console.log(response)
-// 		'X-RapidAPI-Key': 'bd19915a35mshead8ae7b5e30360p1e137bjsned637b7bd883',
-// 		'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
-// 	}
-// };
-const init = () 
+const init = () => {
+    const getJoke = document.querySelector(".btn");
+    getJoke.addEventListener('click', e => {
+        e.preventDefault();
+        const input = document.querySelector('input#joke');
+        console.log(input.value);
+        fetch('https://dad-jokes.p.rapidapi.com/random/joke', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+    }
+}
+document.addEventListener('DOMContentLoaded' init);
+	
+}
+
+
